9de6c1f47066fa72427fa42913c2f7a491ebca6d
John-Rep <replogle.john@next-u.fr>
2025-01-31 02:41:22 +0100

    feat: JWT stored in Cookies

--- API/app.js
+++ API/app.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const cors = require('cors');
 
 mongoose.connect('mongodb://localhost:27017/BoiteAnnonces?retryWrites=true&w=majority')
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -12,8 +13,14 @@ const jwt = require('./middlewares/jwtauth');
 const rateLimiter = require('./middlewares/rateLimiter');
 const upload = require('./middlewares/upload');
 const annonceCont = require('./controllers/annonceController');
+const cookieParser = require('cookie-parser');
 
 app.use(express.json());
+app.use(cors({
+    origin: 'http://127.0.0.1:5500',
+    credentials: true
+  }));
+app.use(cookieParser());
 
 
 app.get('/api/annonces', [jwt.verifyToken, jwt.userExists], annonceCont.getAnnonces)
